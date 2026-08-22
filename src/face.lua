46afdf1c0f4815ab08d0b2eba6a8f70029750667
yournameplease <pokefanjack@proton.me>
2026-08-22 11:08:11 -0500

    additional profiling

--- src/face.lua
+++ src/face.lua
@@ -194,8 +194,7 @@ function Faces:draw_faces(draw_vertices, l)
 
   local drawn = 0
   for i = 0, self.length-1 do
-  -- for i = 0, 0 do
-    profile("face_quad_setup")
+    profile("face_quad_get_row")
     local idx, z = self.sort:get(SORT_IDX_COL, i, 2)
     local v0, v1, v2, v3,
       nx, ny, nz,
@@ -205,22 +204,23 @@ function Faces:draw_faces(draw_vertices, l)
       v3_u, v3_v,
       s
       = self.data:get(0, idx, 16)
+    profile("face_quad_get_row")
 
     if z > 1/FOCAL_LENGTH then
       break
     else
 
-      -- todo: only for drawn lines
-      -- lines_buffer:copy(-1)
-    
+      profile("face_quad_get_verts")
       local x0, y0, z0 = draw_vertices.data:get(0, v0, 3)
       local x1, y1, z1 = draw_vertices.data:get(0, v1, 3)
       local x2, y2, z2 = draw_vertices.data:get(0, v2, 3)
       local x3, y3, z3 = draw_vertices.data:get(0, v3, 3)
+      profile("face_quad_get_verts")
 
       -- backface culling
       -- assume coplanar points
       -- take vector perpindicular to the plane formed by v0->v3, v0->v1 as the normal
+      profile("face_quad_compute_culling")
       local p1 = {x = x3-x0, y = y3-y0, z = (1/z3)-(1/z0)}
       local p2 = {x = x1-x0, y = y1-y0, z = (1/z1)-(1/z0)}
       local n = {
@@ -228,37 +228,40 @@ function Faces:draw_faces(draw_vertices, l)
         y = p1.x * p2.z - p2.x * p1.z,
         z = p1.x * p2.y - p2.x * p1.y,
       }
+      profile("face_quad_compute_culling")
       -- i think since this is camera space I can just dot with (0, 0, -1) and check if positive?
       local n_dot_camera = -n.z
       if n_dot_camera < 0 then
         goto continue
       end
 
-        local points = {
-          [0] = {x = x0, y = y0, w = z0, u = v0_u * z0, v = v0_v * z0},
-          [1] = {x = x1, y = y1, w = z1, u = v1_u * z1, v = v1_v * z1},
-          [2] = {x = x2, y = y2, w = z2, u = v2_u * z2, v = v2_v * z2},
-          [3] = {x = x3, y = y3, w = z3, u = v3_u * z3, v = v3_v * z3},
-        }
+      profile("face_quad_find_min_max_y")
+      local points = {
+        [0] = {x = x0, y = y0, w = z0, u = v0_u * z0, v = v0_v * z0},
+        [1] = {x = x1, y = y1, w = z1, u = v1_u * z1, v = v1_v * z1},
+        [2] = {x = x2, y = y2, w = z2, u = v2_u * z2, v = v2_v * z2},
+        [3] = {x = x3, y = y3, w = z3, u = v3_u * z3, v = v3_v * z3},
+      }
 
-        local y_min = 1e9
-        local y_min_i
-        local y_min_min_x = 1e9
-        local y_min_max_x = 1e9
-        local y_max = -1e9
-        local y_max_i
-    
-        for j = 0,3 do
-          local y = points[j].y
-          if y < y_min then
-            y_min_i = j 
-            y_min = y
-          end
-          if y > y_max then
-            y_max_i = j 
-            y_max = y
-          end
+      local y_min = 1e9
+      local y_min_i
+      local y_min_min_x = 1e9
+      local y_min_max_x = 1e9
+      local y_max = -1e9
+      local y_max_i
+  
+      for j = 0,3 do
+        local y = points[j].y
+        if y < y_min then
+          y_min_i = j 
+          y_min = y
         end
+        if y > y_max then
+          y_max_i = j 
+          y_max = y
+        end
+      end
+      profile("face_quad_find_min_max_y")
 
       if y_min > SCREEN_HEIGHT - 1 then
         goto continue
@@ -267,202 +270,195 @@ function Faces:draw_faces(draw_vertices, l)
         goto continue
       end
         
-        local y_min_i_left = y_min_i 
-        local y_min_i_right = y_min_i 
-        local y_max_i_left = y_max_i
-        local y_max_i_right = y_max_i
+      profile("face_quad_compute_scanlines_min_max")
+      local y_min_i_left = y_min_i 
+      local y_min_i_right = y_min_i 
+      local y_max_i_left = y_max_i
+      local y_max_i_right = y_max_i
 
-        local left_y
-        local right_y
-        local left_y_final = 0
-        local right_y_final = 0
+      local left_y
+      local right_y
+      local left_y_final = 0
+      local right_y_final = 0
 
-        if y_min < 0 then
-          while points[(y_min_i_left + 3) % 4].y < 0 do
-             y_min_i_left = (y_min_i_left + 3) % 4
-          end
-          while points[(y_min_i_right + 1) % 4].y < 0 do
-             y_min_i_right = (y_min_i_right + 1) % 4
-          end
-          local i_next = (y_min_i_right + 1) % 4
-          local i_prev = (y_min_i_left + 3) % 4
-
-          do
-            local p = points[y_min_i_left]
-            local q = points[i_prev]
-            local t = (0 - p.y) / (q.y - p.y)
-            lines_buffer:set(L_X0_COL, 0, t * (q.x - p.x) + p.x)
-            lines_buffer:set(L_U0_COL, 0, t * (q.u - p.u) + p.u)
-            lines_buffer:set(L_V0_COL, 0, t * (q.v - p.v) + p.v)
-            lines_buffer:set(L_W0_COL, 0, t * (q.w - p.w) + p.w)
-          end
-          do
-            local p = points[y_min_i_right]
-            local q = points[i_next]
-            local t = (0 - p.y) / (q.y - p.y)
-            lines_buffer:set(L_X1_COL, 0, t * (q.x - p.x) + p.x)
-            lines_buffer:set(L_U1_COL, 0, t * (q.u - p.u) + p.u)
-            lines_buffer:set(L_V1_COL, 0, t * (q.v - p.v) + p.v)
-            lines_buffer:set(L_W1_COL, 0, t * (q.w - p.w) + p.w)
-          end
-
-          left_y = 0
-          right_y = 0
-        else
-          local p = points[y_min_i]
-          left_y = flr(y_min)
-          right_y = flr(y_min)
-
-          lines_buffer:set(L_X0_COL, left_y, p.x)
-          lines_buffer:set(L_X1_COL, right_y, p.x)
-          lines_buffer:set(L_U0_COL, left_y, p.u)
-          lines_buffer:set(L_V0_COL, left_y, p.v)
-          lines_buffer:set(L_U1_COL, right_y, p.u)
-          lines_buffer:set(L_V1_COL, right_y, p.v)
-          lines_buffer:set(L_W0_COL, left_y, p.w)
-          lines_buffer:set(L_W1_COL, right_y, p.w)
+      if y_min < 0 then
+        while points[(y_min_i_left + 3) % 4].y < 0 do
+           y_min_i_left = (y_min_i_left + 3) % 4
         end
-
-        if y_max > SCREEN_HEIGHT - 1  then
-          while points[(y_max_i_left + 1) % 4].y > SCREEN_HEIGHT - 1 do
-             y_max_i_left = (y_max_i_left + 1) % 4
-          end
-          while points[(y_max_i_right + 3) % 4].y > SCREEN_HEIGHT - 1 do
-             y_max_i_right = (y_max_i_right + 3) % 4
-          end
-          local i_next = (y_max_i_left + 1) % 4
-          local i_prev = (y_max_i_right + 3) % 4
-
-          do
-            local p = points[y_max_i_left]
-            local q = points[i_next]
-            local t = (SCREEN_HEIGHT - 1 - p.y) / (q.y - p.y)
-            lines_buffer:set(L_X0_COL, SCREEN_HEIGHT - 1, t * (q.x - p.x) + p.x)
-            lines_buffer:set(L_U0_COL, SCREEN_HEIGHT - 1, t * (q.u - p.u) + p.u)
-            lines_buffer:set(L_V0_COL, SCREEN_HEIGHT - 1, t * (q.v - p.v) + p.v)
-            lines_buffer:set(L_W0_COL, SCREEN_HEIGHT - 1, t * (q.w - p.w) + p.w)
-          end
-          do
-            local p = points[y_max_i_right]
-            local q = points[i_prev]
-            local t = (SCREEN_HEIGHT - 1 - p.y) / (q.y - p.y)
-            lines_buffer:set(L_X1_COL, SCREEN_HEIGHT - 1, t * (q.x - p.x) + p.x)
-            lines_buffer:set(L_U1_COL, SCREEN_HEIGHT - 1, t * (q.u - p.u) + p.u)
-            lines_buffer:set(L_V1_COL, SCREEN_HEIGHT - 1, t * (q.v - p.v) + p.v)
-            lines_buffer:set(L_W1_COL, SCREEN_HEIGHT - 1, t * (q.w - p.w) + p.w)
-          end
-
-          left_y_final = SCREEN_HEIGHT - 1
-          right_y_final = SCREEN_HEIGHT - 1
-        else
-          local p = points[y_max_i]
-          left_y_final = flr(y_max)
-          right_y_final = flr(y_max)
-
-          lines_buffer:set(L_X0_COL, left_y_final, p.x)
-          lines_buffer:set(L_X1_COL, right_y_final, p.x)
-          lines_buffer:set(L_U0_COL, left_y_final, p.u)
-          lines_buffer:set(L_V0_COL, left_y_final, p.v)
-          lines_buffer:set(L_U1_COL, right_y_final, p.u)
-          lines_buffer:set(L_V1_COL, right_y_final, p.v)
-          lines_buffer:set(L_W0_COL, left_y_final, p.w)
-          lines_buffer:set(L_W1_COL, right_y_final, p.w)
+        while points[(y_min_i_right + 1) % 4].y < 0 do
+           y_min_i_right = (y_min_i_right + 1) % 4
         end
+        local i_next = (y_min_i_right + 1) % 4
+        local i_prev = (y_min_i_left + 3) % 4
 
-        y_min = flr(y_min)
-        y_max = flr(y_max)
-
-        -- fill left endpoints
-        -- go ccw until reached max point
         do
-          local j = y_min_i_left
-          while true do
-            j = (j + 3) % 4
-            if j == y_max_i_left then
-              local len = left_y_final - left_y
-              lines_buffer:lerp(left_y * L_LEN + L_X0_COL, len, L_LEN, 1)
-              lines_buffer:lerp(left_y * L_LEN + L_U0_COL, len, L_LEN, 1)
-              lines_buffer:lerp(left_y * L_LEN + L_V0_COL, len, L_LEN, 1)
-              lines_buffer:lerp(left_y * L_LEN + L_W0_COL, len, L_LEN, 1)
-              break
-            else
-              local left_y_next = flr(points[j].y)
-              local len = left_y_next - left_y
-              lines_buffer:set(L_X0_COL, left_y_next, points[j].x)
-              lines_buffer:set(L_U0_COL, left_y_next, points[j].u)
-              lines_buffer:set(L_V0_COL, left_y_next, points[j].v)
-              lines_buffer:set(L_W0_COL, left_y_next, points[j].w)
-              lines_buffer:lerp(left_y * L_LEN + L_X0_COL, len, L_LEN, 1)
-              lines_buffer:lerp(left_y * L_LEN + L_U0_COL, len, L_LEN, 1)
-              lines_buffer:lerp(left_y * L_LEN + L_V0_COL, len, L_LEN, 1)
-              lines_buffer:lerp(left_y * L_LEN + L_W0_COL, len, L_LEN, 1)
-              left_y = left_y_next
-            end
-          end
+          local p = points[y_min_i_left]
+          local q = points[i_prev]
+          local t = (0 - p.y) / (q.y - p.y)
+          lines_buffer:set(L_X0_COL, 0, t * (q.x - p.x) + p.x)
+          lines_buffer:set(L_U0_COL, 0, t * (q.u - p.u) + p.u)
+          lines_buffer:set(L_V0_COL, 0, t * (q.v - p.v) + p.v)
+          lines_buffer:set(L_W0_COL, 0, t * (q.w - p.w) + p.w)
         end
-
-        -- fill right endpoints
-        -- go cw until reached max point
         do
-          local j = y_min_i_right
-          while true do
-            j = (j + 1) % 4
-            if j == y_max_i_right then
-              local len = right_y_final - right_y
-              lines_buffer:lerp(right_y * L_LEN + L_X1_COL, len, L_LEN, 1)
-              lines_buffer:lerp(right_y * L_LEN + L_U1_COL, len, L_LEN, 1)
-              lines_buffer:lerp(right_y * L_LEN + L_V1_COL, len, L_LEN, 1)
-              lines_buffer:lerp(right_y * L_LEN + L_W1_COL, len, L_LEN, 1)
-              break
-            else
-              local right_y_next = flr(points[j].y)
-              local len = right_y_next - right_y
-              lines_buffer:set(L_X1_COL, right_y_next, points[j].x)
-              lines_buffer:set(L_U1_COL, right_y_next, points[j].u)
-              lines_buffer:set(L_V1_COL, right_y_next, points[j].v)
-              lines_buffer:set(L_W1_COL, right_y_next, points[j].w)
-              lines_buffer:lerp(right_y * L_LEN + L_X1_COL, len, L_LEN, 1)
-              lines_buffer:lerp(right_y * L_LEN + L_U1_COL, len, L_LEN, 1)
-              lines_buffer:lerp(right_y * L_LEN + L_V1_COL, len, L_LEN, 1)
-              lines_buffer:lerp(right_y * L_LEN + L_W1_COL, len, L_LEN, 1)
-              right_y = right_y_next
-            end
-          end
+          local p = points[y_min_i_right]
+          local q = points[i_next]
+          local t = (0 - p.y) / (q.y - p.y)
+          lines_buffer:set(L_X1_COL, 0, t * (q.x - p.x) + p.x)
+          lines_buffer:set(L_U1_COL, 0, t * (q.u - p.u) + p.u)
+          lines_buffer:set(L_V1_COL, 0, t * (q.v - p.v) + p.v)
+          lines_buffer:set(L_W1_COL, 0, t * (q.w - p.w) + p.w)
         end
 
-        local len = y_max - y_min
-      profile("face_quad_setup")
+        left_y = 0
+        right_y = 0
+      else
+        local p = points[y_min_i]
+        left_y = flr(y_min)
+        right_y = flr(y_min)
 
-        local nf = vector_normalize({x = nx, y = ny, z = nz})
-        n_dot_l = nf.x * l.x + nf.y * l.y + nf.z * l.z
-        -- ambient = 0.1
-        -- 0.1 reserved for specular, if I get to it...
-        n_dot_l = n_dot_l * 0.8 + 0.1
-        lighting_ramp:set_lighting_table(n_dot_l)
-
-        -- color(0xC000)
-        profile("face_texture")
-        lines_buffer:copy(s, true, L_S_COL, L_S_COL, 1, L_LEN, L_LEN, SCREEN_HEIGHT)
-
-        
-
-        
-        tline3d(lines_buffer, L_LEN * y_min, len, 12, L_LEN)    
-        profile("face_texture")
-
-
-        profile("face_lighting")
-        do -- lighting
-
-        -- profile("face_lighting_lines")
-            -- line(lines_buffer, L_X0_COL + L_LEN * y_min, len, 4, L_LEN)
-        -- profile("face_lighting_lines")
-          lighting_ramp:clear()
-        end
-        profile("face_lighting")
-       
-        drawn = drawn + 1
+        lines_buffer:set(L_X0_COL, left_y, p.x)
+        lines_buffer:set(L_X1_COL, right_y, p.x)
+        lines_buffer:set(L_U0_COL, left_y, p.u)
+        lines_buffer:set(L_V0_COL, left_y, p.v)
+        lines_buffer:set(L_U1_COL, right_y, p.u)
+        lines_buffer:set(L_V1_COL, right_y, p.v)
+        lines_buffer:set(L_W0_COL, left_y, p.w)
+        lines_buffer:set(L_W1_COL, right_y, p.w)
       end
+
+      if y_max > SCREEN_HEIGHT - 1  then
+        while points[(y_max_i_left + 1) % 4].y > SCREEN_HEIGHT - 1 do
+           y_max_i_left = (y_max_i_left + 1) % 4
+        end
+        while points[(y_max_i_right + 3) % 4].y > SCREEN_HEIGHT - 1 do
+           y_max_i_right = (y_max_i_right + 3) % 4
+        end
+        local i_next = (y_max_i_left + 1) % 4
+        local i_prev = (y_max_i_right + 3) % 4
+
+        do
+          local p = points[y_max_i_left]
+          local q = points[i_next]
+          local t = (SCREEN_HEIGHT - 1 - p.y) / (q.y - p.y)
+          lines_buffer:set(L_X0_COL, SCREEN_HEIGHT - 1, t * (q.x - p.x) + p.x)
+          lines_buffer:set(L_U0_COL, SCREEN_HEIGHT - 1, t * (q.u - p.u) + p.u)
+          lines_buffer:set(L_V0_COL, SCREEN_HEIGHT - 1, t * (q.v - p.v) + p.v)
+          lines_buffer:set(L_W0_COL, SCREEN_HEIGHT - 1, t * (q.w - p.w) + p.w)
+        end
+        do
+          local p = points[y_max_i_right]
+          local q = points[i_prev]
+          local t = (SCREEN_HEIGHT - 1 - p.y) / (q.y - p.y)
+          lines_buffer:set(L_X1_COL, SCREEN_HEIGHT - 1, t * (q.x - p.x) + p.x)
+          lines_buffer:set(L_U1_COL, SCREEN_HEIGHT - 1, t * (q.u - p.u) + p.u)
+          lines_buffer:set(L_V1_COL, SCREEN_HEIGHT - 1, t * (q.v - p.v) + p.v)
+          lines_buffer:set(L_W1_COL, SCREEN_HEIGHT - 1, t * (q.w - p.w) + p.w)
+        end
+
+        left_y_final = SCREEN_HEIGHT - 1
+        right_y_final = SCREEN_HEIGHT - 1
+      else
+        local p = points[y_max_i]
+        left_y_final = flr(y_max)
+        right_y_final = flr(y_max)
+
+        lines_buffer:set(L_X0_COL, left_y_final, p.x)
+        lines_buffer:set(L_X1_COL, right_y_final, p.x)
+        lines_buffer:set(L_U0_COL, left_y_final, p.u)
+        lines_buffer:set(L_V0_COL, left_y_final, p.v)
+        lines_buffer:set(L_U1_COL, right_y_final, p.u)
+        lines_buffer:set(L_V1_COL, right_y_final, p.v)
+        lines_buffer:set(L_W0_COL, left_y_final, p.w)
+        lines_buffer:set(L_W1_COL, right_y_final, p.w)
+      end
+      profile("face_quad_compute_scanlines_min_max")
+
+      y_min = flr(y_min)
+      y_max = flr(y_max)
+
+      -- fill left endpoints
+      -- go ccw until reached max point
+      profile("face_quad_compute_scanlines_left")
+      do
+        local j = y_min_i_left
+        while true do
+          j = (j + 3) % 4
+          if j == y_max_i_left then
+            local len = left_y_final - left_y
+            lines_buffer:lerp(left_y * L_LEN + L_X0_COL, len, L_LEN, 1)
+            lines_buffer:lerp(left_y * L_LEN + L_U0_COL, len, L_LEN, 1)
+            lines_buffer:lerp(left_y * L_LEN + L_V0_COL, len, L_LEN, 1)
+            lines_buffer:lerp(left_y * L_LEN + L_W0_COL, len, L_LEN, 1)
+            break
+          else
+            local left_y_next = flr(points[j].y)
+            local len = left_y_next - left_y
+            lines_buffer:set(L_X0_COL, left_y_next, points[j].x)
+            lines_buffer:set(L_U0_COL, left_y_next, points[j].u)
+            lines_buffer:set(L_V0_COL, left_y_next, points[j].v)
+            lines_buffer:set(L_W0_COL, left_y_next, points[j].w)
+            lines_buffer:lerp(left_y * L_LEN + L_X0_COL, len, L_LEN, 1)
+            lines_buffer:lerp(left_y * L_LEN + L_U0_COL, len, L_LEN, 1)
+            lines_buffer:lerp(left_y * L_LEN + L_V0_COL, len, L_LEN, 1)
+            lines_buffer:lerp(left_y * L_LEN + L_W0_COL, len, L_LEN, 1)
+            left_y = left_y_next
+          end
+        end
+      end
+      profile("face_quad_compute_scanlines_left")
+
+      -- fill right endpoints
+      -- go cw until reached max point
+      profile("face_quad_compute_scanlines_right")
+      do
+        local j = y_min_i_right
+        while true do
+          j = (j + 1) % 4
+          if j == y_max_i_right then
+            local len = right_y_final - right_y
+            lines_buffer:lerp(right_y * L_LEN + L_X1_COL, len, L_LEN, 1)
+            lines_buffer:lerp(right_y * L_LEN + L_U1_COL, len, L_LEN, 1)
+            lines_buffer:lerp(right_y * L_LEN + L_V1_COL, len, L_LEN, 1)
+            lines_buffer:lerp(right_y * L_LEN + L_W1_COL, len, L_LEN, 1)
+            break
+          else
+            local right_y_next = flr(points[j].y)
+            local len = right_y_next - right_y
+            lines_buffer:set(L_X1_COL, right_y_next, points[j].x)
+            lines_buffer:set(L_U1_COL, right_y_next, points[j].u)
+            lines_buffer:set(L_V1_COL, right_y_next, points[j].v)
+            lines_buffer:set(L_W1_COL, right_y_next, points[j].w)
+            lines_buffer:lerp(right_y * L_LEN + L_X1_COL, len, L_LEN, 1)
+            lines_buffer:lerp(right_y * L_LEN + L_U1_COL, len, L_LEN, 1)
+            lines_buffer:lerp(right_y * L_LEN + L_V1_COL, len, L_LEN, 1)
+            lines_buffer:lerp(right_y * L_LEN + L_W1_COL, len, L_LEN, 1)
+            right_y = right_y_next
+          end
+        end
+      end
+      profile("face_quad_compute_scanlines_right")
+
+      local len = y_max - y_min
+
+      profile("face_texture")
+      local nf = vector_normalize(vec(nx, ny, nz))
+      n_dot_l = nf.x * l.x + nf.y * l.y + nf.z * l.z
+      -- ambient = 0.1
+      -- 0.1 reserved for specular, if I get to it...
+      n_dot_l = n_dot_l * 0.8 + 0.1
+      lighting_ramp:set_lighting_table(n_dot_l)
+
+      -- color(0xC000)
+      lines_buffer:copy(s, true, L_S_COL, L_S_COL, 1, L_LEN, L_LEN, SCREEN_HEIGHT)
+      
+      tline3d(lines_buffer, L_LEN * y_min, len, 12, L_LEN)    
+
+      lighting_ramp:clear()
+      profile("face_texture")
+     
+      drawn = drawn + 1
+    end
     ::continue::
   end
 
